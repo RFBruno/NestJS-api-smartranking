fec13dfd732b9b26b51b032efd1ee6f2242c9c4a
RFBruno <bruno_rafael_08@hotmail.com>
2022-06-12 19:13:50 -0300

    added categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 import env from './env';
 
 @Module({
   imports: [
     MongooseModule.forRoot(env.URL_DB,{}),
-    JogadoresModule
+    JogadoresModule,
+    CategoriasModule
   ],
   controllers: [],
   providers: [],
---
+++ src/categorias/interfaces/categoria.interface.ts
@@ -0,0 +1,16 @@
+import { Document } from 'mongoose'
+import { Jogador } from 'src/jogadores/interfaces/jogador.interface';
+export interface Categoria extends Document{
+
+    readonly categoria: string;
+    descricao: string;
+    eventos: Evento[];
+    jogadores: Jogador[];
+
+}
+
+export interface Evento{
+    nome: string;
+    opercao: string;
+    valor: number;
+}
